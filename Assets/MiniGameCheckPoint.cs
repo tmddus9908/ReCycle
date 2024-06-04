c34101e7d2477f9711478d26dbc2c3ff1c8fa953
GameNight <tmddus9908@naver.com>
2024-06-04 11:50:36 +0900

    make minigame

---
+++ Assets/MiniGameCheckPoint.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameCheckPoint : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (transform.position.x > 0)
+        {
+            Minigame.Instance.vec.x *= -1;
+        }
+        else
+        {
+            Minigame.Instance.vec.x *= -1;
+        }
+    }
+}
